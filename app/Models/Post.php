2bcb64cb22632ea6303fcd3384a7b5b4a8b390f4
Zubs <zubairidrisaweda@gmail.com>
2021-09-08 18:09:25 +0100

    refactored

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -23,11 +23,7 @@ class Post
     }
 
     public static function find ($slug) {
-        if (!file_exists($path = resource_path("/posts/" . $slug . ".html"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember('post.' . $slug, now()->addDays(3), fn() => file_get_contents($path));
+        return static::all()->firstWhere('slug', $slug);
     }
 
     public static function all () {
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,1 +1,3 @@
-{!! $post !!}
+<h1>{{ $post->title  }}</h1>
+{!! $post->body !!}
+<p>{{ $post->date }}</p>
