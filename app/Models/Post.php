9c6f36f4566d797ef19d693280d9e75449c6db1f
Zubs <zubairidrisaweda@gmail.com>
2021-09-08 17:11:11 +0100

    Meta and views routes

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,13 +11,15 @@ class Post
     public $excerpt;
     public $date;
     public $body;
+    public $slug;
 
-    public function __construct($title, $excerpt, $date, $body)
+    public function __construct($title, $excerpt, $date, $body, $slug)
     {
         $this->title = $title;
         $this->excerpt = $excerpt;
         $this->date = $date;
         $this->body = $body;
+        $this->slug = $slug;
     }
 
     public static function find ($slug) {
@@ -40,10 +42,11 @@ class Post
                 'body' => $document->body(),
                 'excerpt' => $document->matter('excerpt'),
                 'date' => $document->matter('date'),
+                'slug' => $document->matter('slug'),
             ];
         }
 
-        ddd($posts);
+        return $posts;
 
         return array_map(function ($file) {
             return $file->getContents();
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,7 +4,11 @@
     </head>
     <body>
         @foreach($posts as $post)
-            {!! $post !!}
+            <h1>
+                <a href="/post/{{ $post['slug'] }}">{{ $post['title'] }}</a>
+            </h1>
+            {!! $post['excerpt'] !!}
+            <p>{{ $post['date'] }}</p>
         @endforeach
     </body>
 </html>
