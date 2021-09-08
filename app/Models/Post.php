c60dabf3e467f149440a6828d3b93b1affe564df
Zubs <zubairidrisaweda@gmail.com>
2021-09-08 16:57:54 +0100

    Added meta info

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,9 +3,23 @@
 namespace App\Models;
 
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+
+    public function __construct($title, $excerpt, $date, $body)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+    }
+
     public static function find ($slug) {
         if (!file_exists($path = resource_path("/posts/" . $slug . ".html"))) {
             throw new ModelNotFoundException();
@@ -16,6 +30,20 @@ class Post
 
     public static function all () {
         $files = File::files(resource_path("posts"));
+        $posts = [];
+
+        foreach ($files as $file) {
+            $document = YamlFrontMatter::parseFile($file);
+
+            $posts[] = [
+                'title' => $document->matter('title'),
+                'body' => $document->body(),
+                'excerpt' => $document->matter('excerpt'),
+                'date' => $document->matter('date'),
+            ];
+        }
+
+        ddd($posts);
 
         return array_map(function ($file) {
             return $file->getContents();
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,17 +6,5 @@
         @foreach($posts as $post)
             {!! $post !!}
         @endforeach
-{{--        <div>--}}
-{{--            <h1><a href="post/second-post">Second Post</a></h1>--}}
-{{--            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aspernatur expedita laborum numquam odio provident, sint temporibus veniam? Architecto distinctio earum fuga itaque libero molestias quae quaerat quod recusandae tenetur.</p>--}}
-{{--        </div>--}}
-{{--        <div>--}}
-{{--            <h1><a href="post/post-three">Post Three</a></h1>--}}
-{{--            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aspernatur expedita laborum numquam odio provident, sint temporibus veniam? Architecto distinctio earum fuga itaque libero molestias quae quaerat quod recusandae tenetur.</p>--}}
-{{--        </div>--}}
-{{--        <div>--}}
-{{--            <h1><a href="post/post-after-the-third">Post After The Third</a></h1>--}}
-{{--            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aspernatur expedita laborum numquam odio provident, sint temporibus veniam? Architecto distinctio earum fuga itaque libero molestias quae quaerat quod recusandae tenetur.</p>--}}
-{{--        </div>--}}
     </body>
 </html>
