0538d1dd537f06b85d5cb49ef98e1ebdc38b17ac
Zubs <zubairidrisaweda@gmail.com>
2021-09-22 11:01:44 +0100

    Dynamic components

---
+++ resources/views/components/post-grid.blade.php
@@ -0,0 +1,11 @@
+@props(['posts'])
+
+<x-post-featured-card :post="$posts[0]"></x-post-featured-card>
+
+@if($posts->count > 1)
+    <div class="lg:grid lg:grid-cols-6">
+        @foreach($posts->skip(0) as $post)
+            <x-post-card class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}"></x-post-card>
+        @endforeach
+    </div>
+@endif
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,20 +2,10 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card></x-post-featured-card>
-
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card></x-post-card>
-
-            <x-post-card></x-post-card>
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card></x-post-card>
-
-            <x-post-card></x-post-card>
-
-            <x-post-card></x-post-card>
-        </div>
+        @if($posts->count())
+            <x-post-grid :posts="$posts"></x-post-grid>
+        @else
+            <p class="text-center">No posts yet. Please check back later</p>
+        @endif
     </main>
 </x-layout>
