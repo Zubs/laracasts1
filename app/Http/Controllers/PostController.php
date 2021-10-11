ab6e3707a2b33f739a8980c43676e7c92da84e20
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2021-10-11 14:40:33 +0100

    Added categories model and relationship

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -37,15 +37,9 @@ class PostController extends Controller
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Post  $post
-     * @return \Illuminate\Http\Response
-     */
     public function show(Post $post)
     {
-        //
+        return $post;
     }
 
     /**
