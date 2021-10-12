050b0b534c6b133c3a32fe655395230bfc2ced45
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2021-10-12 15:38:03 +0100

    Dynamic single post view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -40,7 +40,7 @@ class PostController extends Controller
 
     public function show(Post $post)
     {
-        return $post;
+        return view('post')->with('post', $post->load('author', 'category'));
     }
 
     public function edit(Post $post)
