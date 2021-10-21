826c42e08769487f1cd6ba6499a6b7078ca97cc3
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2021-10-21 14:37:43 +0100

    Cleanup

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,10 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Category;
 use App\Models\Post;
-use App\Models\User;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -17,28 +14,8 @@ class PostController extends Controller
         return view('posts.index')->with('posts', $posts);
     }
 
-    public function store(Request $request)
-    {
-        //
-    }
-
     public function show(Post $post)
     {
         return view('posts.show')->with('post', $post->load('author', 'category'));
-    }
-
-    public function edit(Post $post)
-    {
-        //
-    }
-
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
-
-    public function destroy(Post $post)
-    {
-        //
     }
 }
