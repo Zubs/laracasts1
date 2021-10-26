728b4da0fc08fcdf1c5193c5621f4ac5cf9972cf
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2021-10-26 14:21:05 +0100

    Comment form design

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -50,6 +50,23 @@
             </div>
 
             <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                <form method="POST" class="border border-gray-200 p-6 rounded-xl">
+                    @csrf
+
+                    <header class="flex items-center">
+                        <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="Random avatar" width="40" height="40" class="rounded-full">
+                        <h2 class="ml-4">Want to participate?</h2>
+                    </header>
+
+                    <div class="mt-6">
+                        <textarea rows="5" class="w-full text-sm focus:outline-none focus:ring" placeholder="Quick, think of something to say!"></textarea>
+                    </div>
+
+                    <div class="flex justify-end mt-6 border-t border-gray-200 pt-6">
+                        <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
+                    </div>
+                </form>
+
                 @foreach($post->comments as $comment)
                     <x-comment :comment="$comment"></x-comment>
                 @endforeach
